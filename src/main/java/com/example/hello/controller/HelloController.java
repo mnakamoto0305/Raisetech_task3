5b109924720a27af7bb7f17f3ccccec361daa03d
Masahiro Nakamoto <m.nakamoto0305@gmail.com>
2021-02-05 20:06:33 +0900

    styleの設定

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -1,17 +1,18 @@
 package com.example.hello.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
-	
+
    @RequestMapping(value = "/", method = RequestMethod.GET)
-   public String index(Model model) {
-       model.addAttribute("message", "Hello Spling!!");
-       return "index";
+   public ModelAndView index(ModelAndView mav) {
+	   mav.setViewName("index");
+       mav.addObject("hello", "Hello World!");
+       return mav;
    }
-   
+
 }
